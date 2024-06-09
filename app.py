fa25592bf195f2b7567f916799861ced4ca4a67e
KashyapBalapala <kabalapala@gmail.com>
2024-06-09 18:17:26 +0530

    Changes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,8 +13,20 @@ def home():
 def about():
     return "about page"
 
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == "POST":
+        name = request.form['name']
+        message = request.form['message']
+        return f"Received: Name - {name}, Message - {message}"
+    return render_template('form.html')
+
+
+# @app.route('/submit', methods=['POST'])
+# def submit():
+#     name = request.form['name']
+#     message = request.form['message']
+#     return f"Received: Name - {name}, Message - {message}"
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
